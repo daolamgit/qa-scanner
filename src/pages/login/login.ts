e27ceea61f79bb6c6106b9e3d9bdce2cb6c126aa
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2018-02-20 12:52:29 -0600

    Ionic configuration.

---
+++ src/pages/login/login.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  showSpinner: boolean = false;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  login() {
+    this.showSpinner = !this.showSpinner;
+  }
+}
